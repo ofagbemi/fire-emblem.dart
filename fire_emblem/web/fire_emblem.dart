d00ddcd4698f58078432230371b7ab3a7428862a
Jare Fagbemi <ofagbemi@stanford.edu>
2014-08-01 19:23:04 -0400

    Re-added fire_emblem.dart

---
+++ fire_emblem/web/fire_emblem.dart
@@ -0,0 +1,96 @@
+import 'package:mapper/mapper.dart';
+import 'package:unit/unit.dart';
+
+import 'package:fire_emblem/chapter.dart';
+
+import 'dart:html';
+import 'dart:async';
+
+void main() {
+  BodyElement body = querySelector('body');
+
+  ImageElement sacaeTilesetImg = new ImageElement(src: 'images/tilesets/sacae1.png');
+  sacaeTilesetImg.className = 'spritesheet';
+
+  CanvasElement mapCanvas = querySelector('#map');
+  CanvasElement rangeCanvas = querySelector('#range');
+  CanvasElement overworldCanvas = querySelector('#overworld');
+
+  List<CanvasElement> canvases = [mapCanvas, rangeCanvas, overworldCanvas];
+
+  int tileSize = 32;
+
+  canvases.forEach((c) {
+    c.width = 15 * tileSize;
+    c.height = 10 * tileSize;
+    c.context2D.imageSmoothingEnabled = false;
+  });
+
+  Chapter ch = new Chapter(
+      'json/test_chapter.json',
+      mapCanvas, rangeCanvas, overworldCanvas,
+      tileSize, tileSize
+  );
+  ch.loadChapter();
+
+  Unit getUnitAtTile(Point p) {
+    return ch.map.getTileXY(p.x, p.y).properties['unit'];
+  }
+
+  Unit selectedUnit;
+  window.onKeyDown.listen((KeyboardEvent e) {
+    var cursor = ch.cursor;
+    switch(e.keyCode) {
+      case KeyCode.UP:
+        cursor.up();
+
+        // ch.up();  // TODO
+
+        break;
+      case KeyCode.DOWN:
+        cursor.down();
+        break;
+      case KeyCode.LEFT:
+        cursor.left();
+        break;
+      case KeyCode.RIGHT:
+        cursor.right();
+        break;
+      case KeyCode.Z:
+        if(selectedUnit != null) {
+          ch.cursor.lock();
+          selectedUnit.currentPath = ch.map.getPathToTile(
+              selectedUnit.currentTilePointRounded,
+              cursor.currentTilePointRounded,
+              filter: selectedUnit.stats['move']
+          );
+        } else {
+          selectedUnit = getUnitAtTile(cursor.currentTilePointRounded);
+          if(selectedUnit != null) {
+            selectedUnit.selected = true;
+            selectedUnit.setSprite('overworld', 'down');
+            ch.map.drawRange(selectedUnit.getRange());
+          }
+        }
+        break;
+    }
+  });
+
+  int frame = 0;
+  void update(Timer t) {
+    if(!ch.loaded) return;
+
+    if(ch.map != null) ch.map.drawSelf();
+
+    ch.overworldCanvas.context2D.clearRect(0, 0, overworldCanvas.width, overworldCanvas.height);
+    ch.entities.forEach((u) {
+      u.move(onDone: () {
+        u.setSprite('overworld', 'down');
+      });
+      u.currentSprite.setAnimationFrame(frame);
+      u.drawSelfAtTile(ch.map, ch.overworldCanvas.context2D);
+    });
+    frame++;
+  }
+  ch.loop(60, update);
+}
